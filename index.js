e11c4aa6d10b0885a8fe1fe506a8b32e0b42f4a5
deepak-ega143 <deepakegastu@gmail.com>
2024-01-06 14:12:54 +0530

    added ports

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express=require("express");
+const {createTodo}=require("./types");
 const app=express();
 app.use(express.json());
 app.post("/todo",function(req,res){
-
+   const createPayload=req.body;
+   const parsedPayload= createTodo.safeParse(createPayload);
+   if(!parsedPayload.success){
+    res.status(411).json({
+      msg:"You sent the wrong inputs",
+    })
+    return;
+   }
 })
 
 app.get("/todos", function(req,res){
